a63295c6078f449a5d642e5323898b078a051f6e
Viktor Draganov <viktor.draganov@elando.com>
2022-04-06 00:19:16 +0300

    Add project files.

---
+++ KubernetesDeployTest/Program.cs
@@ -0,0 +1,42 @@
+using KubernetesDeployTest.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using var serviceScope = app.Services.CreateScope();
+var services = serviceScope.ServiceProvider;
+var data = services.GetRequiredService<ApplicationDbContext>();
+data.Database.Migrate();
+
+// Configure the HTTP request pipeline.
+
+app.UseMigrationsEndPoint();
+app.UseExceptionHandler("/Home/Error");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
